6eb212d66afd3e9f1b9ee1a7e75b6fd00229739c
James Ortiz <jamesortiz@gmail.com>
2026-07-23 00:03:41 +0000

    ajustada mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Filha, eu te amo. Se você me ama, manda um coração!</h1>
+    return <h1>Filha, eu te amo. Se você me ama, manda um coraçãozinhp pra mim no Whatsapp!</h1>
 }
 
 export default Home;
